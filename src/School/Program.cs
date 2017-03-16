2dcc9b3516818f5f0333868ff2e38786e2eb793e
Aaron Popp <a_popp@hotmail.com>
2017-03-16 16:30:59 -0500

    first (actual) commit - Class 6 prep (incomplete)

--- src/School/Program.cs
+++ src/School/Program.cs
@@ -5,6 +5,71 @@ using System.Threading.Tasks;
 
 namespace School
 {
+    public class Student
+    {
+        private static int nextStudentId = 1;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+
+        public Student(string name, int studentId,
+                int numberOfCredits, double gpa)
+        {
+            Name = name;
+            StudentId = studentId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
+
+        public Student(string name, int studentId)
+            : this(name, studentId, 0, 0) { }
+
+        public Student(string name)
+            : this(name, nextStudentId)
+        {
+            nextStudentId++;
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            // Update the appropriate fields: numberOfCredits, gpa
+            
+            NumberOfCredits += courseCredits;
+
+        }
+
+        public string GetGradeLevel()
+        {
+            // Determine the grade level of the student based on numberOfCredits
+            if (NumberOfCredits < 30)
+            {
+                return "Freshman";
+            }
+            else if (NumberOfCredits > 29 || NumberOfCredits < 60)
+            {
+                return "Sophomore";
+            }
+            else if (NumberOfCredits > 59 || NumberOfCredits < 90)
+            {
+                return "Junior";
+            }
+            else
+            {
+                return "Senior";
+            }
+
+        }
+
+    }
+
+    public class Course
+    {
+        private string name;
+        private int courseId;
+        private List<Student> students;
+    }
+
     public class Program
     {
         public static void Main(string[] args)
